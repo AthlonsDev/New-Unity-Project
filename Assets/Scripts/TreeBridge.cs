6f598209787698c8cee47f5021c5c97ea748fa1a
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-06-11 16:46:06 +0100

    Push Mechanics
    Force applied to Object makes it move

--- Assets/Scripts/TreeBridge.cs
+++ Assets/Scripts/TreeBridge.cs
@@ -19,7 +19,9 @@ public class TreeBridge : MonoBehaviour
             {
                 Debug.Log("Pushes Tree");
                 rb.gravityScale = 1f;
-                col.isTrigger = false;
+                col.enabled = true;
+                //TODO: Add a Force to Push it
+                rb.AddForce(new Vector2(100f, 0));
 
             }
         }
