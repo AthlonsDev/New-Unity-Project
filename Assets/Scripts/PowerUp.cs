47e911d33da10835a3d38e8edaeb5a48b9855216
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-07-07 23:13:01 +0200

    Setting Powerup menu UI script

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -32,4 +32,6 @@ public class PowerUp: MonoBehaviour {
         }
     }
 
+    
+
 }
---
+++ Assets/Scripts/PowerUpWheel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpWheel: MonoBehaviour 
+{
+    //set public variables for ui buttons
+
+void private void Update() {
+    
+    // When controller button is pressed(holding)
+    if(input.GetButtonDown("SkillMenu")) 
+    {
+        // Pause Game and Move Around Wheel and select buttons instead
+        
+    }
+
+}
+
+
+
+}
