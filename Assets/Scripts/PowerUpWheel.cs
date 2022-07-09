636dd88055d84638d6416b51525222195911f2df
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-07-09 23:14:38 +0200

    Power Up Menu System Script

--- Assets/Scripts/PowerUpWheel.cs
+++ Assets/Scripts/PowerUpWheel.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public class PowerUpWheel: MonoBehaviour 
 {
-    //set public variables for ui buttons
+    //TODO:set public variables for ui buttons
+
+    public GameObject player;
 
 void private void Update() {
     
     // When controller button is pressed(holding)
-    if(input.GetButtonDown("SkillMenu")) 
+    while(input.GetButtonDown("SkillMenu")) 
     {
+        // Old System
         // Pause Game and Move Around Wheel and select buttons instead
-        
+        //slow time of the game
+        // time.timeScale = 0.2;
+
+        // New System
+        // Select skill with directional buttons
+        // TODO: Set These names to the correpsonding buttons in the input system
+        if(input.GetButtonDown("Up")) {
+            // This selects The Hook 
+            player.GetComponent<PowerUp>().selectedItem = "Hook";
+        }
     }
 
 }
